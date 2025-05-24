7d6aed2eebea2cdd52c8f23f1b4037a530ca8982
Alexander Opalic <opalic@gmx.net>
2025-05-24 11:30:22 +0200

    Replace NuxtRouteAnnouncer and NuxtWelcome components with NuxtPage in app.vue

---
+++ content.config.ts
@@ -0,0 +1,16 @@
+import { defineContentConfig, defineCollection, z } from '@nuxt/content'
+
+export default defineContentConfig({
+  collections: {
+    blog: defineCollection({
+      type: 'page',
+      source: 'blog/**/*.md',
+      schema: z.object({
+        tags: z.array(z.string()),
+        title: z.string(),
+        description: z.string(),
+        date: z.date()
+      })
+    })
+  }
+})
